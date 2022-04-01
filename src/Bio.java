c0a8dd9319e43ef4d39fe0bbc788a7b41b590d51
Sam Dobson <sam.dobsonn@gmail.com>
2022-04-01 12:10:40 +0100

    DNA to RNA Conversion

---
+++ src/Bio.java
@@ -0,0 +1,6 @@
+// Codewars - DNA to RNA Conversion - 8 kyu
+public class Bio {
+    public String dnaToRna(String dna) {
+        return dna;  // Do your magic!
+    }
+}
---
+++ tests/BioTest.java
@@ -0,0 +1,17 @@
+// Codewars - DNA to RNA Conversion - 8 kyu
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class BioTest {
+    @Test
+    public void testDna() throws Exception {
+        Bio b = new Bio();
+        assertEquals("UUUU", b.dnaToRna("TTTT"));
+    }
+
+    @Test
+    public void testDna2() throws Exception {
+        Bio b = new Bio();
+        assertEquals("GCAU", b.dnaToRna("GCAT"));
+    }
+}
