10e66a0c84eb4c1357da3d08c6752bfdfd8bb5fc
Sam Dobson <sam.dobsonn@gmail.com>
2022-04-01 12:35:52 +0100

    2

--- src/Bio.java
+++ src/Bio.java
@@ -1,6 +1,15 @@
 // Codewars - DNA to RNA Conversion - 8 kyu
 public class Bio {
     public String dnaToRna(String dna) {
-        return dna;  // Do your magic!
+        //DNA = GCAT
+        //RNA = GCAU
+        StringBuilder dnaString = new StringBuilder(dna);
+        int i;
+        for (i = 0; i < dnaString.length(); i++) {
+            if (dnaString.charAt(i) == 'T') {
+                dnaString.setCharAt(i, 'U');
+            }
+        }
+        return dnaString.toString();
     }
 }
---
+++ src/GrassHopper.java
@@ -0,0 +1,8 @@
+// Codewars - Grasshopper - Summation - 8 kyu
+public class GrassHopper {
+
+    public static int summation(int n) {
+
+        return 0;
+    }
+}
